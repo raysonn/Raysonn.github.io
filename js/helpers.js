68877f9ad93db30f9a78e10ac7e3cc9948e028c8
raysonn taborda <raysonnsuczecktaborda@gmail.com>
2022-03-21 19:29:45 -0300

    Games / Clear

--- js/helpers.js
+++ js/helpers.js
@@ -1,28 +1,34 @@
-function getLanguage(){
+function getLanguage() {
     var language = navigator.language;
     return language;
 }
 
-function loadSections(){
+function loadSections() {
     $("#portfolio").load("html/sections/sportifolio.html");
     $("#about").load("html/sections/sabout.html");
     $("#contact").load("html/sections/scontacts.html");
     $("#resume").load("html/sections/sresume.html");
 }
 
-function printPdf (data , title ) {
-    const byteCharacters = atob ( data );
-    const byteNumbers = new Array ( byteCharacters.length );
-    for ( let i = 0; i < byteCharacters.length; i++) {
-    byteNumbers [i] = byteCharacters.charCodeAt (i);
+function loadGames() {
+    $(window).on('load', function () {
+        $("#games").load("html/sections/sgames.html");
+    });
+}
+
+function printPdf(data, title) {
+    const byteCharacters = atob(data);
+    const byteNumbers = new Array(byteCharacters.length);
+    for (let i = 0; i < byteCharacters.length; i++) {
+        byteNumbers[i] = byteCharacters.charCodeAt(i);
     }
-    const byteArray = new Uint8Array ( byteNumbers );
-    const file = new Blob ([ byteArray ], { type: 'application/pdf;base64' }) ;
-    const fileURL = URL.createObjectURL ( file );
-    const winPDF = window.open ( fileURL , title );
+    const byteArray = new Uint8Array(byteNumbers);
+    const file = new Blob([byteArray], { type: 'application/pdf;base64' });
+    const fileURL = URL.createObjectURL(file);
+    const winPDF = window.open(fileURL, title);
     // Open PDF
     winPDF.onload = (() => {
-      setTimeout (() => winPDF.document.title = title , 1000) ;
-    }) ;
+        setTimeout(() => winPDF.document.title = title, 1000);
+    });
 }
 
